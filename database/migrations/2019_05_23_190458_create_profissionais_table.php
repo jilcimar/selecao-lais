f33773185f1383dcdbeee2e8dd2e88e399b6e9be
jilcimar <jilcimar.fernandes0267@gmail.com>
2019-05-23 16:14:03 -0300

    tabela de profissionais

---
+++ database/migrations/2019_05_23_190458_create_profissionais_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfissionaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profissionais', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->char('cns','15');
+            $table->date('data_atribuicao');
+            $table->integer('carga_horaria');
+            $table->boolean('sus');
+
+           // FK's
+            $table->integer('cbo_id');
+            $table->integer('tipo_id');
+            $table->integer('vinculacao_id');
+
+            $table->foreign('cbo_id')->references('id')->on('cbos')->onDelete('cascade');
+            $table->foreign('tipo_id')->references('id')->on('tipos')->onDelete('cascade');
+            $table->foreign('vinculacao_id')->references('id')->on('vinculacoes')->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profissionais');
+    }
+}
