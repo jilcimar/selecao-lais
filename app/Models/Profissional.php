a8486096c14d4cbb96ac823c65b76f66735ed3bf
jilcimar <jilcimar.fernandes0267@gmail.com>
2019-05-23 16:25:04 -0300

    relacionamento no model de profissional

--- app/Models/Profissional.php
+++ app/Models/Profissional.php
@@ -9,4 +9,20 @@ class Profissional extends Model
     protected $table = 'profissionais';
 
     protected $fillable = ['nome','cns','data_atribuicao','carga_horaria','sus','cbo_id','tipo_id','vinculacao_id'];
+
+
+    public function cbo()
+    {
+        return $this->belongsTo(Cbo::class, 'cbo_id');
+    }
+    public function tipo()
+    {
+        return $this->belongsTo(Tipo::class, 'tipo_id');
+    }
+
+    public function vinculacao ()
+    {
+        return $this->belongsTo(Vinculacao::class, 'vinculacao_id');
+    }
+
 }
