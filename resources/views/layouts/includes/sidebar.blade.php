747b0cdec8fc59e8a2aa32797e7cdbdc0a835d46
jilcimar <jilcimar.fernandes0267@gmail.com>
2019-05-30 16:12:38 -0300

    scraper + crud + grafico

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,73 +1,75 @@
 <section>
-        <!-- Left Sidebar -->
-        <aside id="leftsidebar" class="sidebar">
-            <!-- User Info -->
-            <div class="user-info">
-                <div class="image">
-                    <img src="{{asset('images/user.png')}}" width="48" height="48" alt="User" />
-                </div>
-                <div class="info-container">
-                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</div>
-                    <div class="email">{{ Auth::user()->email }}</div>
-                    <div class="btn-group user-helper-dropdown">
-                        <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
-                        <ul class="dropdown-menu pull-right">
-                            <li><a href="javascript:void(0);"><i class="material-icons">person</i>Perfil</a></li>
-                            <li role="seperator" class="divider"></li>
-                            <li>
-                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <i class="material-icons">input</i>Sair
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
+    <!-- Left Sidebar -->
+    <aside id="leftsidebar" class="sidebar">
+        <!-- User Info -->
+        <div class="user-info">
+            <div class="image">
+                <img src="{{asset('images/user.png')}}" width="48" height="48" alt="User"/>
+            </div>
+            <div class="info-container">
+                <div class="name" data-toggle="dropdown" aria-haspopup="true"
+                     aria-expanded="false">{{ Auth::user()->name }}</div>
+                <div class="email">{{ Auth::user()->email }}</div>
+                <div class="btn-group user-helper-dropdown">
+                    <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                    <ul class="dropdown-menu pull-right">
+                        <li><a href="javascript:void(0);"><i class="material-icons">person</i>Perfil</a></li>
+                        <li role="seperator" class="divider"></li>
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <i class="material-icons">input</i>Sair
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <!-- #User Info -->
-            <!-- Menu -->
-            <div class="menu">
-                <ul class="list">
-                    <li class="header">NAVEGAÇÃO PRINCIPAL</li>
-                    <li class="active">
-                        <a href="{{ url('/') }}">
-                            <i class="material-icons">home</i>
-                            <span>Início</span>
-                        </a>
-                    </li>
+        </div>
+        <!-- #User Info -->
+        <!-- Menu -->
+        <div class="menu">
+            <ul class="list">
+                <li class="header">NAVEGAÇÃO PRINCIPAL</li>
+                <li class="active">
+                    <a href="{{ url('/') }}">
+                        <i class="material-icons">home</i>
+                        <span>Início</span>
+                    </a>
+                </li>
 
-                    <li class="{{ request()->is('profissionais*') ? 'active' : '' }}">
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <i class="material-icons">people</i>
-                            <span>Profissionais</span>
-                        </a>
-                        <ul class="ml-menu">
-                            <li class="{{ request()->is('profissionais/create') ? 'active' : '' }}">
-                                <a href="{{url('profissionais/create')}}">Cadastrar</a>
-                            </li>
-                            <li class="{{ request()->is('profissionais') ? 'active' : '' }}">
-                                <a href="{{url('profissionais')}}">Listar</a>
-                            </li>
-                            <li>
-                                <a href="{{route('importar_dados')}}">Importar dados</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                <li class="{{ request()->is('profissionais*') ? 'active' : '' }}">
+                    <a href="javascript:void(0);" class="menu-toggle">
+                        <i class="material-icons">people</i>
+                        <span>Profissionais</span>
+                    </a>
+                    <ul class="ml-menu">
+                        <li class="{{ request()->is('profissionais/create') ? 'active' : '' }}">
+                            <a href="{{url('profissionais/create')}}">Cadastrar</a>
+                        </li>
+                        <li class="{{ request()->is('profissionais') ? 'active' : '' }}">
+                            <a href="{{url('profissionais')}}">Listar</a>
+                        </li>
+                        <li>
+                            <a href="{{route('importar_dados')}}">Importar dados</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- #Menu -->
+        <!-- Footer -->
+        <div class="legal">
+            <div class="copyright">
+                &copy; 2019 <a href="javascript:void(0);">Seleção do LAIS</a>.
             </div>
-            <!-- #Menu -->
-            <!-- Footer -->
-            <div class="legal">
-                <div class="copyright">
-                    &copy; 2019 <a href="javascript:void(0);">Seleção do LAIS</a>.
-                </div>
-                <div class="version">
-                    <b>Version: </b> 1.0.0
-                </div>
+            <div class="version">
+                <b>Version: </b> 1.0.0
             </div>
-            <!-- #Footer -->
-        </aside>
-        <!-- #END# Left Sidebar -->
-    </section>
+        </div>
+        <!-- #Footer -->
+    </aside>
+    <!-- #END# Left Sidebar -->
+</section>
